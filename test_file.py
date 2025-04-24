57e342b2ed4fd0636aef33329453b7c8f411f7d1
Bharath <bharathkumarkuppala@gmail.com>
2025-04-24 23:18:22 +0530

    added volume for mysql

--- test_file.py
+++ test_file.py
@@ -1,16 +1,14 @@
 from kafka import KafkaConsumer
 import json
 
-# Kafka consumer setup
 consumer = KafkaConsumer(
     'dbserver1.testdb.employees',
     bootstrap_servers='localhost:9092',
-    auto_offset_reset='latest',
-    group_id='test-consumer',
+    auto_offset_reset='earliest',
+    group_id='debug-consumer',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
 )
 
-print("Waiting for messages...\n")
-
 for msg in consumer:
-    print("==== New message received ====")
-    print(msg.value)
+    print("\n🟢 Received message:")
+    print(json.dumps(msg.value, indent=2))
