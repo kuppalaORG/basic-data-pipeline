0f7c1094d8c9c7cbb59edd9126ae3f56372bf4fa
bharathkuppala <bharath.kuppala@simplifyvms.com>
2025-07-25 15:01:01 +0530

    added multipe tables and enable auto commit

--- consumer_to_file.py
+++ consumer_to_file.py
@@ -7,7 +7,7 @@ topic = 'dbserver1.testdb.employees'
 # ✅ Kafka consumer with deserializer
 consumer = KafkaConsumer(
     topic,
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers=['172.31.93.31:9092', 'localhost:9092'],
     group_id='debug-connection-test-' + str(int(__import__('time').time())),
     auto_offset_reset='earliest',
     enable_auto_commit=False,
